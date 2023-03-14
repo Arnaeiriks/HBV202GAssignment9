f794377547c6f0f9491839c513b2abb00d999cc1
Arnaeiriks <are45@hi.is>
2023-03-14 11:14:28 +0000

    breyta PlusComposite

--- src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
+++ src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
@@ -3,7 +3,7 @@ package is.hi.hbv202g.ass9.composite;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlusComposite {
+public class PlusComposite implements MathExpression {
     private List<MathExpression> children = new ArrayList<MathExpression>();
 
     public int getResult() {
